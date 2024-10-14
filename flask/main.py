977762863cc96f9705e2cbd11e2e8c0d210dabfb
Wiktor Owczarek <tikolu43@gmail.com>
2024-10-14 13:12:20 +0100

    Set up hosting and functions with firebase

---
+++ flask/main.py
@@ -0,0 +1,18 @@
+from firebase_admin import initialize_app, db
+from firebase_functions import https_fn
+from flask import render_template, Flask
+
+app = Flask(__name__, template_folder="../views")
+
+@app.route("/")
+def index():
+	return render_template("login.html")
+
+
+
+# Firebase stuff: #
+initialize_app()
+@https_fn.on_request()
+def server(req: https_fn.Request) -> https_fn.Response:
+	with app.request_context(req.environ):
+		return app.full_dispatch_request()
