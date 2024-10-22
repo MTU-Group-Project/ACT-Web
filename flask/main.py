8489d5830eba47b4c55d7f363c4a56b52f3176a5
unknown <alex.ringhoffer@slin.ie>
2024-10-22 21:13:37 +0100

    Share tracker using yfinance

--- flask/main.py
+++ flask/main.py
@@ -2,6 +2,8 @@ from flask import Flask, render_template, redirect
 from firebase_admin import initialize_app, db
 from firebase_functions import https_fn
 from hashlib import sha256
+import json
+import shares
 
 app = Flask(__name__)
 
@@ -21,8 +23,23 @@ def register():
 	return render_template("register.html")
 
 
+@app.get("/api/shares")
+def api_shares():
+	res = app.response_class(
+		response=json.dumps(list(shares.get_share_information().values()), default=lambda s: vars(s)),
+		status=200,
+		mimetype="application/json"
+	)
+
+	return res
+
+
+shares.begin_share_subroutine(120)
+
+
 # Firebase stuff: #
 initialize_app()
+
 @https_fn.on_request(timeout_sec=5)
 def server(req: https_fn.Request) -> https_fn.Response:
 	global request
